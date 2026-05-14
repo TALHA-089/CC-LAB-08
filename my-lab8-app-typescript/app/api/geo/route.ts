1e279f639212e3bc151d42fc1f3f766cb1d00bb9
TALHA-089 <rajag47786@gmail.com>
2026-05-15 01:45:41 +0500

    A small patch

--- my-lab8-app-typescript/app/api/geo/route.ts
+++ my-lab8-app-typescript/app/api/geo/route.ts
@@ -3,9 +3,12 @@ export const runtime = "edge";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+  const country = req.headers.get("x-vercel-ip-country") || "Only available on production";
+  const city = req.headers.get("x-vercel-ip-city") || "Test on your deployed URL";
+
   return NextResponse.json({
-    country: req.geo?.country || "Only available on production",
-    city: req.geo?.city || "Test on your deployed URL",
+    country,
+    city,
     runtime: "edge"
   });
 }
