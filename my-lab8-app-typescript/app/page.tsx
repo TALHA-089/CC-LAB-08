877bafe19f590d532c4bd8e97be5de4e29f9c0f5
TALHA-089 <rajag47786@gmail.com>
2026-05-15 00:01:16 +0500

    Add Lab 8 TypeScript app

---
+++ my-lab8-app-typescript/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "10vh" }}>
+      <h1>Lab 8 - Vercel Deployment</h1>
+      <p>Name: [Talha Naveed]</p>
+      <p>Roll: [01-131232-089]</p>
+    </main>
+  );
+}
